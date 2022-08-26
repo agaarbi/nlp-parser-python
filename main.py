20675fb806d54c8af3f67d47af14b98e6e4be81d
ghani <agaarbi@gmail.com>
2022-08-26 10:32:24 +0500

    comments and proper naming of variables

--- main.py
+++ main.py
@@ -9,28 +9,32 @@ nlp = en_core_web_sm.load()
 query = "Putin and Imran Khan Met in Russia before the Ukranian War in february 2022"
 print(query)
 
-doc = nlp(query)
+nlp_query = nlp(query)
 query_ner = entities.Entities()
 
+# named entity sorting in 5 groups (check docs for each label's descriptive name)
 persons_labels = ['PERSON']
 groups_labels = ['NORP']
 locations_labels = ['FAC', 'ORG', 'GPE', 'LOC']
 events_labels = ['EVENT']
 time_references_labels = ['DATE', 'TIME']
 
-for X in doc:
-    if X.ent_type_ in persons_labels:
-        query_ner.persons = X.text
-    elif X.ent_type_ in groups_labels:
-        query_ner.groups = X.text
-    elif X.ent_type_ in locations_labels:
-        query_ner.locations = X.text
-    elif X.ent_type_ in events_labels:
-        query_ner.events = X.text
-    elif X.ent_type_ in time_references_labels:
-        query_ner.time_references = X.text
+for entry in nlp_query:
+    if entry.ent_type_ in persons_labels:
+        query_ner.persons = entry.text
+    elif entry.ent_type_ in groups_labels:
+        query_ner.groups = entry.text
+    elif entry.ent_type_ in locations_labels:
+        query_ner.locations = entry.text
+    elif entry.ent_type_ in events_labels:
+        query_ner.events = entry.text
+    elif entry.ent_type_ in time_references_labels:
+        query_ner.time_references = entry.text
 
 
+# create json query in required format
+# json dump is used to create json from the entity class
+# other changes are made as per the requirement
 json_str = json.dumps(query_ner.__dict__)
 
 json_str = "{ \"Entities\": " + json_str + "}"
